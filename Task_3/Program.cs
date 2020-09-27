5fb8d6d16c4df2ccc00ddb0166224e0185b0a479
Карина Котулич <karina.kotulich@gmail.com>
2020-09-27 22:43:58 +0300

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+            while (true) {
+                try
+                {
+                    Console.WriteLine();
+                    string a = Console.ReadLine();
+                    int x = Convert.ToInt32(a);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("gjhgj");
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    string b = Console.ReadLine();
+                    double y = Convert.ToDouble(b);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("gjhgj");
+                }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    string c = Console.ReadLine();
+                    long z = Convert.ToInt64(c);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("gjhgj");
+                }
+            }
+            Console.WriteLine($"a = {x},b = {y},c = {z}");
+        }
+    }
+}
