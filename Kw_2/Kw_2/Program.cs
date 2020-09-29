4aec2515ef8164aab18e8dfb8150e16338890ce7
Karin <60923415+Warai-kiki@users.noreply.github.com>
2020-09-29 23:04:51 +0300

    commit try

---
+++ Kw_2/Kw_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kw_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+           
+            Console.WriteLine("Введіть число типу int");
+            string x = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            
+            Console.WriteLine("Введіть число типу double");
+            string y= Console.ReadLine();
+            double b = Convert.ToDouble(y);
+            
+            
+            Console.WriteLine("Введіть число типу long");
+            string z = Console.ReadLine();
+            long c = Convert.ToInt64(z);
+            
+            
+            Console.WriteLine($"a = {a},b = {b},c = {c}");
+
+        }
+    }
+}
