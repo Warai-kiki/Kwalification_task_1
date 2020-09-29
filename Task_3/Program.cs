557f3eec9b626f59a50a4e42bf1ae3d5fff7f347
Карина Котулич <karina.kotulich@gmail.com>
2020-09-29 22:08:07 +0300

    commit 1

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -12,42 +12,42 @@ namespace Task_3
             while (true) {
                 try
                 {
-                    Console.WriteLine();
+                    Console.WriteLine("Введіть число типу int");
                     string a = Console.ReadLine();
                     int x = Convert.ToInt32(a);
                     break;
                 }
                 catch
                 {
-                    Console.WriteLine("gjhgj");
+                    Console.WriteLine("Виникла помилка. Повторіть спробу");
                 }
             }
             while (true)
             {
                 try
                 {
-                    Console.WriteLine();
+                    Console.WriteLine("Введіть число типу double");
                     string b = Console.ReadLine();
                     double y = Convert.ToDouble(b);
                     break;
                 }
                 catch
                 {
-                    Console.WriteLine("gjhgj");
+                    Console.WriteLine("Виникла помилка. Повторіть спробу");
                 }
             }
             while (true)
             {
                 try
                 {
-                    Console.WriteLine();
+                    Console.WriteLine("Введіть число типу long");
                     string c = Console.ReadLine();
                     long z = Convert.ToInt64(c);
                     break;
                 }
                 catch
                 {
-                    Console.WriteLine("gjhgj");
+                    Console.WriteLine("Виникла помилка. Повторіть спробу");
                 }
             }
             Console.WriteLine($"a = {x},b = {y},c = {z}");
